eff1e4b55200b35b63c7c0bc056dbaf60e6119e1
Vinill <danimuller758@gmail.com>
2022-04-28 20:03:05 +0000

    Fixed error 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 
     names = dir(hidden_4)
 
-    for i in range(len(names)):
-        if not (names[i].startswith("__")):
-            print(names[i])
+    for name in names:
+        if names[:2] != "__":
+            print(names)
