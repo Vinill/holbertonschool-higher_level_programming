103b6d13de575e1f0f7a2eeea8722e31087aede1
Vinill <danimuller758@gmail.com>
2022-05-02 15:41:05 +0000

    Arreglo inesperado

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,10 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > range(len(my_list) - 1):
+    elif idx > len(my_list):
         return my_list
     else:
-        for i in my_list:
+        for i in range(len(my_list)):
             if i == idx:
                 my_list[i] = element
                 return my_list
