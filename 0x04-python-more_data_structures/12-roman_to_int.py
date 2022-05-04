0cccafa000097d84e6b35028e7c8d0db2e4292ae
Vinill <danimuller758@gmail.com>
2022-05-04 22:25:04 +0000

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+             'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+             'CD': 400, 'CM': 900}
+    i = 0
     num = 0
+    res = isinstance(roman_string, str)
 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+    if res is False or roman_string is None:
+        return 0
 
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-
+    while i < len(roman_string):
+        if i+1 < len(roman_string) and roman_string[i:i+2] in roman:
+            num += roman[roman_string[i:i+2]]
+            i += 2
         else:
-            num += roman_dict[roman_string[i]]
-    return (num)
+            num += roman[roman_string[i]]
+            i += 1
+    return num
