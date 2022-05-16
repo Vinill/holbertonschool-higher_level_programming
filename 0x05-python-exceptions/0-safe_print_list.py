05bbfec334a0ac316ee38e2c865648fed1f7df7c
Vinill <danimuller758@gmail.com>
2022-05-16 12:25:57 -0300

    pruebas 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+from tkinter import Y
+
+
 def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            return(x)
+    except IndexError:
+        print("")
