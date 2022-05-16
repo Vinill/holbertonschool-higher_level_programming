92870718e060d14b3bc319574f1ebc2e7e57b648
Vinill <danimuller758@gmail.com>
2022-05-16 17:43:16 -0300

    hola

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         func = fct(*args)
         return func
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
