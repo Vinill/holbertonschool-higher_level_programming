dfe25bac86293c96de11f6587945f7416d9bef08
Vinill <danimuller758@gmail.com>
2022-05-17 20:52:29 -0300

    prueba 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Empty class"""
 class Square:
+    """class Square that defines a square by 2"""
     def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
