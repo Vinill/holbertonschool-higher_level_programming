7211dacf715aea5eb83c084755838ef5782b4c1d
Vinill <danimuller758@gmail.com>
2022-05-18 22:19:21 +0000

    g

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -42,3 +42,13 @@ class Rectangle:
             return 0
         else:
             return((self.height * 2) + (self.width * 2))
+    
+        def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        temp = []
+        for x in range(self.height):
+            temp.append("#" * self.width)
+            if x < (self.height - 1):
+                temp.append('\n')
+        return "".join(temp)
