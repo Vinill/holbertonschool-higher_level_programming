90245f3c6d4c2b6557c66cc2f400aeb43c59bd4b
Vinill <danimuller758@gmail.com>
2022-05-17 21:04:10 -0300

    h

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,5 +21,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    """Class Square that defines 
+        a square by 3"""
     def area(self):
         return(self.__size * self.__size)
