7c1bf8e3cdc62ae777f30bd4a3f1ae02e9cd9d2a
Vinill <danimuller758@gmail.com>
2022-05-18 22:12:52 +0000

    fi

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """CLass"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integerr")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -33,7 +33,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """CLass"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integerr")
         if value < 0:
             raise ValueError("height must be >= 0")
