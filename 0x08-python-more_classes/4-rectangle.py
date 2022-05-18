2ad2e2e85c45093b42cb2bdc81851012ee598da7
Vinill <danimuller758@gmail.com>
2022-05-18 22:48:47 +0000

    fixed 4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+
+    """Class Rectangle that defines a rectangle by"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integerr")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integerr")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return(self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return((self.height * 2) + (self.width * 2))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        temp = []
+        for x in range(self.height):
+            temp.append("#" * self.width)
+            if x < (self.height - 1):
+                temp.append('\n')
+        return "".join(temp)
+
+    def __repr__(self):
+        return("Rectangle({:d},{:d})".format(self.__width, self.__height))
