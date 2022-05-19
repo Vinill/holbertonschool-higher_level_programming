f5b4b98fa5f7b12ee00d70f0c9fbe1dfd4f19a2b
Vinill <danimuller758@gmail.com>
2022-05-19 03:35:06 +0000

    h

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,8 +3,9 @@
 
 
 class Rectangle:
-    number_of_instances = 0
     """Class Rectangle that defines a rectangle by"""
+    number_of_instances = 0
+
 
     def __init__(self, width=0, height=0):
         self.width = width
