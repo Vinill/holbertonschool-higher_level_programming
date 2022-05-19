67d95e078115f8d598fb1e95e3da8abae78479ec
Vinill <danimuller758@gmail.com>
2022-05-19 00:33:42 +0000

    f

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -58,5 +58,6 @@ class Rectangle:
         return(f"Rectangle({self.__width}, {self.__height})")
 
     def __del__(self):
+        if Rectangle.number_of_instances > 0:
+            Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
