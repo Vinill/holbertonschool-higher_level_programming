3dc5250818c0964f1314651b8ba6d94845f7b18b
Vinill <danimuller758@gmail.com>
2022-05-24 19:21:08 -0300

    d

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""Class"""
 class LockedClass:
-    """class LockedClass with no class or object attribute"""
+    """
+    class LockedClass with no class or object attribute
+    """
+
     __slots__ = ['first_name']
