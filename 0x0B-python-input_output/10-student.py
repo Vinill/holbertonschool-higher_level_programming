5e7e4509716fab3da275f2e84ce923e3ef5bb494
Vinill <danimuller758@gmail.com>
2022-05-31 13:42:42 -0700

    Fixed 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,10 +6,18 @@ class Student that defines a student by
 
 class Student():
     """Def class Student"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            return(vars(self))
+    def to_json(self, attrs=None):
+        """Def to json"""
+        if attrs is None:
+            return self.__dict__
+        dict1 = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dict1[key] = value
+        return dict1
