35529f313efeb04f154c8b89d1cc9262a0857d1a
Vinill <danimuller758@gmail.com>
2022-05-30 21:31:30 -0300

    a

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+''' Module to add attribute to a class '''
+
 
 def add_attribute(obj, atr, value):
+    ''' Adds an attribute to a class '''
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     else:
