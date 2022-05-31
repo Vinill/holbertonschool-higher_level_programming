ba3ed22af5bdbf6619be989bdc0815c018c943bc
Vinill <danimuller758@gmail.com>
2022-05-31 14:34:20 -0700

    More errors in 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-function that creates an Object from a “JSON file”
-"""
+"""asd asd asd"""
 
 from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file_name = "add_item.json"
+filename = "add_item.json"
 
 try:
-    file_7 = load_from_json_file(file_name)
+    load = load_from_json_file(filename)
 except FileNotFoundError:
-    file_7 = []
+    load = []
 
-save_to_json_file(file_7 + argv[1:], file_name)
+save_to_json_file(load + argv[1:], filename)
