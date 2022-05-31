202285ae1029d7d24622b374c900a0a306bf1f87
Vinill <danimuller758@gmail.com>
2022-05-31 14:23:44 -0700

    Fixed 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,17 @@ function that creates an Object from a “JSON file”
 """
 import sys
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        file_7 = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        file_7 = []
-    file_7.extend(sys.argv[1:])
-    save_to_json_file(file_7, 'add_item.json')
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+file_name = "add_item.json"
+obj = [arg for arg in sys.argv if arg != sys.argv[0]]
+
+try:
+    file_7 = load_from_json_file(file_name)
+except FileNotFoundError:
+    save_to_json_file(obj, file_name)
+else:
+    for i in obj:
+        file_7.append(i)
+    save_to_json_file(file_7, file_name)
