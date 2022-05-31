62b369f0d5eca48accb5bf300b815c09cdcaa1ba
Vinill <danimuller758@gmail.com>
2022-05-30 21:26:08 -0300

    hola

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Import 9"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 Rectangle = __import__("9-rectangle").Rectangle
 
 
@@ -11,7 +14,9 @@ class Square(Rectangle):
         self.__size = size
 
     def area(self):
+        ''' Overrides the area method, and returns the area '''
         return(self.__size * self.__size)
 
     def __str__(self):
+        ''' Prints the dimensions of the square '''
         return("[Square] {:d}/{:d}".format(self.__size, self.__size))
