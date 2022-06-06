256cdb53a8d5aea016cda70828bb993b1fbe5616
Vinill <danimuller758@gmail.com>
2022-06-06 19:57:53 +0000

    Push 14

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     
     def to_json_string(list_dictionaries):
         """Def to_json_string"""
-        if list_dictionaries == None or len(list_dictionaries) == 0:
-            return "[]"
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
         else:
             return json.dumps(list_dictionaries)
