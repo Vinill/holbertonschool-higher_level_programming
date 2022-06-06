4179bf7a09f8689d2d338b333fadf9e34efa834c
Vinill <danimuller758@gmail.com>
2022-06-06 19:01:33 +0000

    Fixe pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,8 +2,8 @@
 """ This class will be the “base” of all other classes in this project.
     The goal of it is to manage id attribute in all your future classes
     and to avoid duplicating the same code (by extension, same bugs) """
-    
-    
+
+
 class Base:
     """ class Base """
     __nb_objects = 0
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """ class rectangle """
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ def init """
         self.width = width
@@ -42,12 +42,12 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-    
+
     @property
     def x(self):
         """ property x """
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """ def x """
@@ -61,7 +61,7 @@ class Rectangle(Base):
     def y(self):
         """ property y """
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """ def y """
@@ -70,11 +70,11 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-        
+
     def area(self):
         """ def area """
         return self.__width * self.__height
-    
+
     def display(self):
         """ def display """
         print(f"\n" * self.__y, end="")
@@ -84,7 +84,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """ def str """
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} -\
+ {self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
         """Definition Update"""
