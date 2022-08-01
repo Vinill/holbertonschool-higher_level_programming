7d7c8b581a82dcb85f9605c267ca96d0695730c1
Vinill <danimuller758@gmail.com>
2022-08-01 17:58:19 +0000

    hey bro check this out

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const variable = process.argv;
 if (variable.length === 3) {
   console.log('Argument found');
-} else if (variable < 3) {
+} else if (variable.length < 3) {
   console.log('No argument');
 } else {
   console.log('Arguments found');
