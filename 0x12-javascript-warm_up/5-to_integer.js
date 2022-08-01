3fa584915ef2cd05a32f3fc61c433357237c24ca
Vinill <danimuller758@gmail.com>
2022-08-01 19:27:02 +0000

    fix ej 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const variable = process.argv;
-if (variable.length === 3) {
+if (variable.length === 3 && parseInt(variable[2])) {
   console.log('My number: ' + parseInt(variable[2]));
 } else {
   console.log('Not a number');
