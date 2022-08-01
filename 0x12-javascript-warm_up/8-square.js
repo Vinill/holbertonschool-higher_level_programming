e794d30079c08051078e520706390f6478bd01b1
Vinill <danimuller758@gmail.com>
2022-08-01 20:17:51 +0000

    fix 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArray = Number(process.argv[2]);
+const str = 'X';
+if (parseInt(myArray)) {
+  for (let i = 0; i < myArray; i++) {
+    console.log(str.repeat(myArray));
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
