144f84f76f41d0df5edb51b656b1b67b6ca701e6
Vinill <danimuller758@gmail.com>
2022-08-01 17:55:37 +0000

    ey bro, chequeate esto

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const variable = process.argv;
+if (variable.length === 3) {
+  console.log('Argument found');
+} else if (variable < 3) {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
+}
