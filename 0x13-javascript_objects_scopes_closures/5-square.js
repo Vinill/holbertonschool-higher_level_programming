9fa5dac829b6d964e083d70c9d3495c10bfa9a05
Vinill <danimuller758@gmail.com>
2022-08-02 18:48:58 +0000

    push 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,5 @@
-module.exports = class Square extends require('./4-rectangle.js'){
-    constructor (size) 
-    {
-        super (size, size);
-    }
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
 };
