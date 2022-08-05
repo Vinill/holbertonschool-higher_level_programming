c5a3aad62e83d947837a0aac044b4686f2f70ba8
Vinill <danimuller758@gmail.com>
2022-08-05 19:42:04 +0000

    lets go

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+        cur.execute("SELECT cities.id, states.name, cities.name FROM states, \
+        cities WHERE cities.state_id = states.id ORDER BY cities.id ASC")
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
