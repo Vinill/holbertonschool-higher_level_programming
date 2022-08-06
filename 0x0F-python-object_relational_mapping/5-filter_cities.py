b42eee4388660aa4ac02d236a7d72cadc8e7ac40
Vinill <danimuller758@gmail.com>
2022-08-06 18:40:11 +0000

    fix 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,24 +3,19 @@
 Script that lists all cities from the database hbtn_0e_4_usa
 """
 
+import sys 
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    javi_db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        password=argv[2],
-        database=argv[3],
-        port=3306)
-    cur = javi_db.cursor()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
+    cur = db.cursor()
 
-    cur.execute("SELECT cities.name FROM states \
-    JOIN cities ON cities.state_id = states.id WHERE states.name = %s\
-    ORDER by cities.id", [argv[4]])
+    cur.execute("SELECT cities.name FROM states, \
+    JOIN cities ON cities.state_id = states.id WHERE states.name = %s \
+    ORDER BY cities.id ASC", [argv[4]])
     new = []
+
     for row in cur.fetchall():
-        new += row
-    print(', '.join(new))
-    javi_db.close()
-    cur.close()
+        print(row[0], end=" ," if row != rows[-1] else "\n")
+
+    db.close()
