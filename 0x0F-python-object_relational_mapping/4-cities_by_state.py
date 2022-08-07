b6ab49f90aef8df68edc2d98f4ed816f3c6154d4
Vinill <danimuller758@gmail.com>
2022-08-07 21:50:45 +0000

    ahora

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,9 +5,9 @@ import MySQLdb
 from sys import argv
 
 if (__name__ == "__main__"):
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
