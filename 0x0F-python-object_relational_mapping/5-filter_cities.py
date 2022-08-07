540d5490a8c8bc8daafa3142e05b7433a7f0e1e0
Vinill <danimuller758@gmail.com>
2022-08-07 21:08:46 +0000

    yeah

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,11 +3,16 @@
 Script that lists all cities from the database hbtn_0e_4_usa
 """
 
-from sys import argv 
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2], database=argv[3], port=3306)
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306)
     cur = db.cursor()
 
     cur.execute("SELECT cities.name FROM states, \
