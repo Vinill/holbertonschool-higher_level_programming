4379e38ee5ac91c07c37804e3506bdb659dee9c1
Vinill <danimuller758@gmail.com>
2022-08-07 21:21:10 +0000

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""Task: 0. Get all states"""
+
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    javi_db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        password=argv[2],
-        database=argv[3],
-        port=3306)
-    cur = javi_db.cursor()
+if (__name__ == "__main__"):
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM states \
-    JOIN cities ON cities.state_id = states.id ORDER by cities.id")
-    for row in cur.fetchall():
-        print(row)
-    javi_db.close()
+    cur = con.cursor()
+    qryOne = "SELECT cities.id, cities.name, states.name FROM cities"
+    qryTwo = " INNER JOIN states ON cities.state_id = states.id"
+    cur.execute(qryOne + qryTwo)
+    query = cur.fetchall()
+
+    for x in query:
+        print(x)
     cur.close()
+    con.close()
