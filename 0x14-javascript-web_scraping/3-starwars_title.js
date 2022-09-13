d591533289b75b925df718e51dccd13b1c4a0faf
Vinill <danimuller758@gmail.com>
2022-09-13 02:03:21 +0000

    fix more semistandar

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-function doHeadRequest () {
-    const movie = process.argv[2];
-    const url = `https://swapi-api.hbtn.io/api/films/${movie}`;
-    const axios = require('axios');
-  
-    try {
-        const res = axios.get(url);
-        const data = res.data;
-        console.log(data.title);
-    } catch (error) {
-        console.log(error);
+const axios = require('axios');
+
+async function Request () {
+  try {
+    const res = await axios.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`);
+    const data = res.data;
+    console.log(data.title);
+  } catch (error) {
+    if (error) {
+      console.log(error);
     }
   }
-  doHeadRequest();
-  
+}
+
+Request();
