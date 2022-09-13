868266b9b055a3df54fc5aa2f06aaf4c25633e3b
Vinill <danimuller758@gmail.com>
2022-09-13 01:58:34 +0000

    semistandar fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
 function doHeadRequest () {
-    const argv = process.argv[2];
-    const axios = require('axios');
-  
-    axios.get(argv)
+  const argv = process.argv[2];
+  const axios = require('axios');
+  axios.get(argv)
     .then((response) => {
-                console.log(`code: ${response.status}`);
-            })
-  
-      .catch((error) => {
-        console.log(`code: ${error.response.status}`);
-      });
-    }
-    doHeadRequest();
+      console.log(`code: ${response.status}`);
+    })
+    .catch((error) => {
+      console.log(`code: ${error.response.status}`);
+    });
+}
+doHeadRequest();
