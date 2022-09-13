6fad2f0787a148e735add788b1de09a63f940586
Vinill <danimuller758@gmail.com>
2022-09-13 01:52:37 +0000

    and fix more errors

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,13 +5,12 @@ function doHeadRequest () {
     const axios = require('axios');
   
     axios.get(argv)
-      .then((response) => {
-        console.log(`code: ${response.status}`);
-      })
+    .then((response) => {
+                console.log(`code: ${response.status}`);
+            })
   
       .catch((error) => {
         console.log(`code: ${error.response.status}`);
       });
-  }
-  doHeadRequest();
-  
+    }
+    doHeadRequest();
